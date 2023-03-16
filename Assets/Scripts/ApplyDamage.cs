bd4ebbb19f9ed1ae3d81648095c32da815dd6312
Alexandr <rfst@mail.ru>
2023-03-16 20:01:43 +0700

    Hp bar, score bar

---
+++ Assets/Scripts/ApplyDamage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class ApplyDamage : MonoBehaviour
+    {
+        [SerializeField] private int _damage;
+        [SerializeField] private CreatureDef _defDamage;
+
+        private void Start()
+        {
+            _damage = _defDamage.Damage;
+        }
+
+        public void Apply(GameObject target)
+        {
+            if (target.TryGetComponent<HealthComponent>(out HealthComponent health))
+            {
+                health.ModifyHealth(_damage, gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CreatureDef.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    [CreateAssetMenu(menuName = "Defs/CreatureDef", fileName = "CreatureDef")]
+    public class CreatureDef : ScriptableObject
+    {
+        [SerializeField] [Range(0, 100)] private int _health;
+        [SerializeField] [Range(5, 50)] private int _damage;
+
+        public int Health => _health;
+        public int Damage => _damage;
+    }
+}
